1f2a3e82e150173f1e1527aa5d4241eee77b2d17
Ladislav Lisy <ladislav.lisy@seznam.cz>
2017-08-31 15:09:26 +0200

    docker-deploy

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	data := [][]string{
-		[]string{"Alfred", "15", "10/20", "(10.32, 56.21, 30.25)"},
-		[]string{"Beelzebub", "30", "30/50", "(1,1,1)"},
-		[]string{"Hortense", "21", "80/80", "(1,1,1)"},
-		[]string{"Pokey", "8", "30/40", "(1,1,1)"},
+		[]string{"Maruska", "15", "10/20", "(10.32, 56.21, 30.25)"},
+		[]string{"Nastenka", "30", "30/50", "(1,1,1)"},
+		[]string{"Sengina", "21", "80/80", "(1,1,1)"},
+		[]string{"Jednorozec", "8", "30/40", "(1,1,1)"},
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
